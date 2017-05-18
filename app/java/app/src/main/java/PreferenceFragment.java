04417e2d78c43905617479bbe86a18f4a80773ca
p <koldfuzor@gmail.com>
2017-05-18 09:55:45 -0700

    android build fix

--- app/java/app/src/main/java/PreferenceFragment.java
+++ app/java/app/src/main/java/PreferenceFragment.java
@@ -3,14 +3,22 @@ package com.lucidfusionlabs.app;
 import android.os.*;
 
 public class PreferenceFragment extends android.support.v7.preference.PreferenceFragmentCompat {
+    public static String LAYOUT_ID = "LAYOUT_ID";
     public int layout_id;
-
-    public PreferenceFragment() { layout_id = 0; }
-    public PreferenceFragment(int id) { layout_id = id; }
+    
+    public static final PreferenceFragment newInstance(int lid) {
+        PreferenceFragment ret = new PreferenceFragment();
+        Bundle bundle = new Bundle(1);
+        bundle.putInt(LAYOUT_ID, lid);
+        ret.setArguments(bundle);
+        ret.layout_id = lid;
+        return ret;
+    }
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         super.onCreate(savedInstanceState);
+        layout_id = getArguments().getInt(LAYOUT_ID);
         addPreferencesFromResource(layout_id);
     }
 }
