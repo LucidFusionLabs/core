e866eebae0860f284113cb75290176c6f08711f6
Justin Foutts <jfoutts@tripwire.com>
2018-05-18 11:22:04 -0700

    Update LFLCMake for mac-Entitlements.plist

--- app/framework/null_crashreporting.cpp
+++ app/framework/null_crashreporting.cpp
@@ -1,4 +1,6 @@
+#include "core/app/app.h"
+
 namespace LFL {
-void InitCrashReporting(const string &id) {}
+void InitCrashReporting(const string &id) { FATAL("not implemented"); }
 void TestCrashReporting() {}
 } // namespace LFL
