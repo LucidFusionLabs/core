0209e0a76566a71cce6aede8d313b981a3baba4a
Justin Foutts <jfoutts@tripwire.com>
2018-05-23 06:56:07 -0700

    Move osx specific imports

--- script/AddFabricBuildPhaseToXcodeprojTarget.sh
+++ script/AddFabricBuildPhaseToXcodeprojTarget.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Adding Fabric Run Script Build Phase to $2 target $3 for API token $CRASHLYTICS_API_TOKEN on $1
-core/script/AddRunScriptBuildPhaseToXcodeprojTarget.rb $2 $3 "\$PROJECT_DIR/core/imports/fabric-$1/Crashlytics.framework/run $CRASHLYTICS_API_TOKEN $CRASHLYTICS_BUILD_SECRET"
+core/script/AddRunScriptBuildPhaseToXcodeprojTarget.rb $2 $3 "\$PROJECT_DIR/core/imports/$1/fabric-$1/Crashlytics.framework/run $CRASHLYTICS_API_TOKEN $CRASHLYTICS_BUILD_SECRET"
